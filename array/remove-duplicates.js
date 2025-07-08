1d6ee3003336fd9816e0d6053a9c2ab8b96f5cdc
mzajbe <mzajbe@gmail.com>
2025-07-08 15:03:42 +0600

    complete-remove-duplicates-array

--- array/remove-duplicates.js
+++ array/remove-duplicates.js
@@ -1,5 +1,19 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
 
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
 
-var removeDuplicates = function(nums) {
-    
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1;
 };
+
+let nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+let k2 = removeDuplicates(nums2);
+console.log("Output:", k2, ", nums:", nums2);
